e651471536b20a11c2150a00d197cb0725474322
zachariahbioto3-sketch <zachariahbioto3@gmail.com>
2026-07-01 06:38:55 +0000

    feat: render deployment config, gunicorn, production settings

--- devmarket/settings.py
+++ devmarket/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import environ
+import dj_database_url
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
@@ -70,10 +72,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'devmarket.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3',
+        conn_max_age=600,
+    )
 }
 
 AUTH_USER_MODEL = 'accounts.User'
